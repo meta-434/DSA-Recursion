c314901f380676362bda61bfa9ea1fbdcc6e125e
meta-434 <ach1@vt.edu>
2020-06-02 16:48:49 -0400

    factorial solution

---
+++ solutions/factorial.js
@@ -0,0 +1,20 @@
+
+//
+// const factorial = (n) => {
+//     // base case
+//     if (n <= 1) {
+//         return 1;
+//     }
+//
+//     return n * factorial(n - 1);
+// }
+
+
+
+function factorial (n) {
+    const m = []
+    // if (n <= 1) return 1
+    // if (m[n] > 0) return m[n]
+    // return m[n] = factorial(n - 1) * n
+}
+console.log(factorial(20));
