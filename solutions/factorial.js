5e006ca5ec948210ef36fba05f4b52ee7e2a035c
meta-434 <ach1@vt.edu>
2020-06-04 10:40:37 -0400

    uncomment factorial soln

--- solutions/factorial.js
+++ solutions/factorial.js
@@ -1,20 +1,19 @@
 
-//
-// const factorial = (n) => {
-//     // base case
-//     if (n <= 1) {
-//         return 1;
-//     }
-//
-//     return n * factorial(n - 1);
-// }
+const factorial = (n) => {
+    // base case
+    if (n <= 1) {
+        return 1;
+    }
 
-
-
-function factorial (n) {
-    const m = []
-    // if (n <= 1) return 1
-    // if (m[n] > 0) return m[n]
-    // return m[n] = factorial(n - 1) * n
+    return n * factorial(n - 1);
 }
-console.log(factorial(20));
+
+// memoization solution
+
+// function factorial (n) {
+//     const m = []
+//     // if (n <= 1) return 1
+//     // if (m[n] > 0) return m[n]
+//     // return m[n] = factorial(n - 1) * n
+// }
+console.log(factorial(7));
